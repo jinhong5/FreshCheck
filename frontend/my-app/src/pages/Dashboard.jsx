73486c2c853a08ebd22b1b5071d42c3497fc21a2
elainexing <elainexing99@gmail.com>
2026-03-14 19:12:14 -0400

    ui background

--- frontend/my-app/src/pages/Dashboard.jsx
+++ frontend/my-app/src/pages/Dashboard.jsx
@@ -111,12 +111,12 @@ export default function DashboardPage() {
 
                   const left = daysLeft(item.date, item.expiryDate);
                   return (
-                    <tr key={item.id}>
+                    <tr key={item.id} style={{ backgroundColor: left <= 1 ? "var(--warning)" : "inherit", color: left <= 1 ? "var(--text-primary)" : "inherit" }}>
                       <td><img src={item.photourl} alt={item.label} width={100} /></td>
                       <td>{item.label}</td>
                       <td>{item.category}</td>
                       <td>{formatDateTime(item.expiryDate)}</td>
-                      <td style={{ color: left <= 1 ? "red" : "inherit", fontWeight: left <= 1 ? "bold" : "normal" }}>
+                      <td>
                         {left}</td>
                     </tr>
                   )
