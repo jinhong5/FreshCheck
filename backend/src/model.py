0b748d487c485d843877eae5fff78c60d6549689
ACXing5 <ACXing5@gmail.com>
2026-03-14 19:04:38 -0400

    Trained model

--- backend/src/model.py
+++ backend/src/model.py
@@ -1,23 +1,23 @@
-import torch.nn as nn
-import torchvision.models as models
+# import torch.nn as nn
+# import torchvision.models as models
 
 
-class FruitExpirationModel(nn.Module):
+# class FruitExpirationModel(nn.Module):
 
-    def __init__(self):
-        super().__init__()
+#     def __init__(self):
+#         super().__init__()
 
-        # pretrained backbone
-        self.backbone = models.resnet18(weights="IMAGENET1K_V1")
+#         # pretrained backbone
+#         self.backbone = models.resnet18(weights="IMAGENET1K_V1")
 
-        in_features = self.backbone.fc.in_features
+#         in_features = self.backbone.fc.in_features
 
-        # replace final layer for regression
-        self.backbone.fc = nn.Sequential(
-            nn.Linear(in_features, 256),
-            nn.ReLU(),
-            nn.Linear(256, 1)
-        )
+#         # replace final layer for regression
+#         self.backbone.fc = nn.Sequential(
+#             nn.Linear(in_features, 256),
+#             nn.ReLU(),
+#             nn.Linear(256, 1)
+#         )
 
-    def forward(self, x):
-        return self.backbone(x)
+#     def forward(self, x):
+#         return self.backbone(x)
