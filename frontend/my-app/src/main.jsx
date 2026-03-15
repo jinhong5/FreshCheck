a674cc2e88879a2a84a9d1a2efa46aaaf29dce3f
ACXing5 <ACXing5@gmail.com>
2026-03-14 23:05:57 -0400

    Fixed UserContext rename bug

--- frontend/my-app/src/main.jsx
+++ frontend/my-app/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { UserProvider } from './components/userContext.jsx';
+import { UserProvider } from './contexts/UserContext.jsx';
 import { BrowserRouter } from "react-router-dom";
 
 import './index.css'
@@ -9,7 +9,7 @@ import App from './App.jsx'
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-console.log(GOOGLE_CLIENT_ID);
+// console.log(GOOGLE_CLIENT_ID);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
